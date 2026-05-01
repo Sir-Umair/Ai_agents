3ee19b886677a61e113a42a9cbe681363661fc48
Sir-Umair <umairsahib242@gmail.com>
2026-05-02 02:28:30 +0500

    Restructured project: moved Email-marketer/app to app, updated frontend to Next.js, and preserved old vite frontend in frontend_vite

--- Email-marketer/debug_import.py
+++ Email-marketer/debug_import.py
@@ -5,7 +5,7 @@ import traceback
 import os
 
 def f():
-    time.sleep(2)
+    time.sleep(60)
     sys.stderr.write("DUMPING TRACEBACK:\n")
     sys.stderr.write("".join(traceback.format_stack(sys._current_frames()[threading.main_thread().ident])))
     os._exit(1)
